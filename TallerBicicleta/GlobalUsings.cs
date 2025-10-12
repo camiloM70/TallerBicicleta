777130020fd0f604a63bc40d8425fabdc5bb649d
camilo <camilo@CODEMACHINE>
2025-10-12 18:49:11 -0500

    Se agrega el archivo GlobalUsings.cs para centralizar los using globales del proyecto

---
+++ TallerBicicleta/GlobalUsings.cs
@@ -0,0 +1,29 @@
+// GlobalUsings.cs - Gestión centralizada de espacios de nombres
+// Este archivo define los using globales para todo el proyecto TallerBicicleta
+
+// ===== SISTEMA BASE =====
+global using System;
+global using System.Collections.Generic;
+global using System.ComponentModel.DataAnnotations;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Net.Http.Json;
+global using System.Text;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+// ===== ASP.NET CORE =====
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Components.Forms;
+global using Microsoft.AspNetCore.Components.Routing;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.Web.Virtualization;
+
+// ===== BLAZOR ESPECÍFICO =====
+global using Microsoft.JSInterop;
+global using static Microsoft.AspNetCore.Components.Web.RenderMode;
+
+// ===== PROYECTO ESPECÍFICO =====
+global using TallerBicicleta;
+global using TallerBicicleta.Components;
