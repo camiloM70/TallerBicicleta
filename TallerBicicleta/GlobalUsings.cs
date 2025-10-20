df9138a054f1cf36143ca0e1e61fa19a0b3dba8f
camilo <camilo@CODEMACHINE>
2025-10-19 23:11:27 -0500

    using glabales actualizados

--- TallerBicicleta/GlobalUsings.cs
+++ TallerBicicleta/GlobalUsings.cs
@@ -6,25 +6,30 @@ global using System;
 global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
 global using System.Linq;
-global using System.Net.Http;
-global using System.Net.Http.Json;
 global using System.Text;
-global using System.Threading;
 global using System.Threading.Tasks;
+global using System.Security.Cryptography;
+global using System.IO;
 
 // ===== ASP.NET CORE =====
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Authorization;
-global using Microsoft.AspNetCore.Components.Forms;
-global using Microsoft.AspNetCore.Components.Routing;
-global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.Web.Virtualization;
+global using Microsoft.AspNetCore.Mvc;
+global using Route = Microsoft.AspNetCore.Components.RouteAttribute;
 
 // ===== BLAZOR ESPECÍFICO =====
-global using Microsoft.JSInterop;
-global using static Microsoft.AspNetCore.Components.Web.RenderMode;
+global using Microsoft.Extensions.Logging;
 
 // ===== PROYECTO ESPECÍFICO =====
-global using TallerBicicleta;
 global using TallerBicicleta.Components;
 global using TallerBicicleta.Models;
+global using TallerBicicleta.Services;
+global using TallerBicicleta.Data;
+
+// ===== OTROS PAQUETES =====
+global using Google.Cloud.Firestore;
+global using SixLabors.ImageSharp;
+global using SixLabors.ImageSharp.PixelFormats;
+global using ZXing;
+global using ZXing.Common;
+//global using Stripe;
